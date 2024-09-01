aed0efdc5b542e3a346da0604a9d297524645945
Nuntuch_NB7 <nuntuch13589@gmail.com>
2024-09-01 22:45:26 +0700

    CREATE Simple JUniTest

---
+++ src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+	public int addNum(int a, int b) {
+		
+		return a+b;
+		
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/example/demo/service/UnitTestTestService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UnitTestTestService {
+
+	@InjectMocks
+	private TestService testService;
+	
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		
+		assertEquals(testService.addNum(1,1), 2);
+	}
+
+}
