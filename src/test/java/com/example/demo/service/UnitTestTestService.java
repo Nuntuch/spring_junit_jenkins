e180a331032b71327fa883a512866ccaad2449e0
Nuntuch_NB7 <nuntuch13589@gmail.com>
2024-09-02 13:57:34 +0700

    REMOVE JUnit4 import

--- src/test/java/com/example/demo/service/UnitTestTestService.java
+++ src/test/java/com/example/demo/service/UnitTestTestService.java
@@ -4,11 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
